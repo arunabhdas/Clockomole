a96e416371a280926f14de3ea75274e7cc798e42
Arunabh Das <arunabhdas@gmail.com>
2023-01-06 00:56:06 -0500

    Complted Part 10

--- CodeLabsPro/CodeLabsPro/CodeLabsProApp.swift
+++ CodeLabsPro/CodeLabsPro/CodeLabsProApp.swift
@@ -4,15 +4,31 @@ import SwiftUI
 
 @main
 struct CodeLabsProApp: App {
-    @State private var scrums = DailyScrum.sampleData
+    
+    @StateObject private var store = ScrumStore()
     var body: some Scene {
         WindowGroup {
             // MeetingView()
             // CardView(scrum: DailyScrum.sampleData[0]).background(DailyScrum.sampleData[0].theme.mainColor)
             
             NavigationView {
-                ScrumsView(scrums: $scrums)
-                    .background(DailyScrum.sampleData[0].theme.mainColor)
+                ScrumsView(scrums: $store.scrums) {
+                    ScrumStore.save(scrums: store.scrums) { result in
+                        if case .failure(let error) = result {
+                            fatalError(error.localizedDescription)
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                ScrumStore.load { result in
+                    switch result {
+                    case .failure(let error):
+                        fatalError(error.localizedDescription)
+                    case .success(let scrums):
+                        store.scrums = scrums
+                    }
+                }
             }
         }
     }
